7d208833276a2cf47e26eee30199bde71c132d40
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-07 17:33:51 +0200

    Add Java-File and Add code to the java-file

---
+++ DAY2_Datatypes/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        String Summe = "Summe";
+        int Zahl1 = 3;
+        int Zahl2 = 5;
+
+        System.out.println(addieren(Zahl1, Zahl2));
+
+    }
+
+    public static int addieren(int a, int b){
+
+        return a + b;
+
+    }
+
+}
